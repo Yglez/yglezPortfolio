5d1c34dad154f1b6e4ed846f225ff19395f1e176
Yesenia <yglez787@gmail.com>
2019-01-21 12:43:41 -0800

    fixed responsive column issue

--- src/components/Work.js
+++ src/components/Work.js
@@ -12,22 +12,16 @@ class Work extends Component {
   render() {
     const { workData } = this.state;
     return (
-      <div className="cards">
-        {workData.map((data, i) => {
-          return <WorkCard key={data.id} data={data} />;
-        })}
-        {/*         {workData.map((data, i) => {
-          if ((i + 1) % 4 === 0) {
-            return (
-              <div className="card-deck" key={data.id}>
-                <WorkCard key={data.id} data={data} />
-              </div>
-            );
-          } else {
-            return <WorkCard key={data.id} data={data} />;
-          }
-        })} */}
-      </div>
+      <React.Fragment>
+        <div className="container my-5">
+          <div className="row">
+            {workData.map((data, i) => {
+              return <WorkCard key={data.id} data={data} />;
+            })}
+          </div>
+          >
+        </div>
+      </React.Fragment>
     );
   }
 }
--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -5,23 +5,34 @@ class WorkCard extends Component {
   render() {
     const { projectSite, projectName, img, githubLink } = this.props.data;
     return (
-      <div className="card">
-        <img src={img} className="card-img-top" alt="projects" />
-        <div className="card-body">
-          <a href={projectSite} className="btn-light">
-            <FontAwesomeIcon className="icon0x" icon={["fas", "eye"]} />
-            {projectName}
-          </a>
-          <div className="card-footer">
-            <small className="text-muted">
-              <a href={githubLink} className="btn-dark">
-                <FontAwesomeIcon className="icon0x" icon={["fab", "github"]} />
-                GitHub
+      <React.Fragment>
+        <div className="col-10 mx-auto col-md-6 col-lg-4 my-3">
+          <div className="card">
+            <img
+              src={img}
+              className="img-card-top"
+              style={{ height: "18rem" }}
+              alt="projects pic"
+            />
+            <div className="card-body text-capitalize" />
+            <div className="card-footer">
+              <a href={projectSite} className="btn-light">
+                <FontAwesomeIcon className="icon0x" icon={["fas", "eye"]} />
+                {projectName}
               </a>
-            </small>
+              <small className="text-muted">
+                <a href={githubLink} className="btn-dark">
+                  <FontAwesomeIcon
+                    className="icon0x"
+                    icon={["fab", "github"]}
+                  />
+                  GitHub
+                </a>
+              </small>
+            </div>
           </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
