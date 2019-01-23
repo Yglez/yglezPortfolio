8b809e411f72131ec30e3011ad9311b03f9e35ff
Yesenia <yglez787@gmail.com>
2019-01-23 10:50:18 -0800

    updated links

--- src/components/dataApi/workData.js
+++ src/components/dataApi/workData.js
@@ -1,46 +1,46 @@
 const workData = [
   {
-    id: 0,
+    id: 1,
     projectName: "Business Solutions",
-    projectSite: "https://nostalgic-cori-603c17.netlify.com/",
+    projectSite: "https://yglez-businesssolutions.netlify.com",
     img: "./img/businessSolutions.JPG",
     githubLink: "https://github.com/Yglez/deecalcestimate"
   },
   {
-    id: 1,
-    projectName: "Movie Search",
-    projectSite: "https://nostalgic-montalcini-f399dc.netlify.com/movies",
-    img: "./img/kidsMovies.JPG",
-    githubLink: "https://github.com/Yglez/reactPj"
-  },
-  {
     id: 2,
-    projectName: "To do App",
-    projectSite: "https://nostalgic-montalcini-f399dc.netlify.com/todo",
-    img: "./img/toDo.JPG",
-    githubLink: "https://github.com/Yglez/reactPj"
-  },
-  {
-    id: 3,
     projectName: "NYC Restaurants",
-    projectSite: "https://nostalgic-montalcini-f399dc.netlify.com/",
+    projectSite: "https://yglez-nycrestaurants.netlify.com",
     img: "./img/restaurantsNYC.JPG",
     githubLink: "https://github.com/Yglez/reactPj"
   },
   {
-    id: 4,
+    id: 3,
     projectName: "Recipe Box",
-    projectSite: "https://optimistic-heyrovsky-ffa515.netlify.com/",
+    projectSite: "https://yglez-recipebox.netlify.com",
     img: "./img/recipes.JPG",
     githubLink: "https://github.com/Yglez/recipesApp"
   },
   {
-    id: 5,
+    id: 4,
     projectName: "HacktoberFest Contribution",
     projectSite:
       "https://zero-to-mastery.github.io/Halloween-Hacktoberfest/index.html",
     img: "./img/halloween.JPG",
     githubLink: "https://github.com/Yglez/Halloween-Hacktoberfest"
+  },
+  {
+    id: 5,
+    projectName: "Movie Search",
+    projectSite: "https://yglez-nycrestaurants.netlify.com/movies",
+    img: "./img/kidsMovies.JPG",
+    githubLink: "https://github.com/Yglez/reactPj"
+  },
+  {
+    id: 6,
+    projectName: "To do App",
+    projectSite: "https://yglez-nycrestaurants.netlify.com/todo",
+    img: "./img/toDo.JPG",
+    githubLink: "https://github.com/Yglez/reactPj"
   }
 ];
 export default workData;
